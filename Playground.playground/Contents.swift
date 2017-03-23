b9abb97d9561a62598400810614f06789547912f
Rafal Piekarski <ravbaker@gmail.com>
2017-03-24 00:16:42 +0100

    Playground: first attempt to fetch content from server

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,5 +1,23 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-var str = "Hello, playground"
+var fileUrl = "http://pub.rejestrymedyczne.csioz.gov.pl/pobieranie_WS/Pobieranie.ashx?filetype=XMLUpdateFile&regtype=RPL_FILES_GROWTH"
+
+let theUrl = URL(string: fileUrl)
+
+let task = URLSession.shared.dataTask(with: theUrl!) { (data, response, error) in
+  if let error = error {
+    print(error)
+  } else {
+    if let content = data {
+      response?.className
+      print(response?.suggestedFilename ?? "no filename")
+      (response as! HTTPURLResponse).allHeaderFields
+    }
+  }
+  PlaygroundPage.current.finishExecution()
+}
+task.resume()
