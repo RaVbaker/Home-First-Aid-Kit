048ea06288f382270102420329276a361be029ea
Rafal Piekarski <ravbaker@gmail.com>
2017-03-24 01:01:31 +0100

    first version with different dataSources

--- PL meds DB/PL meds DB/main.swift
+++ PL meds DB/PL meds DB/main.swift
@@ -61,14 +61,45 @@ class MedsParser {
   }
 }
 
+class ConsoleApp {
+  let basePath = "file:///Users/ravbaker/Code/Home-First-Aid-Kit"
+  
+  var list: [Medicine] = []
+  
+  init(_ args: [String]) {
+    var source = "default"
+    if args.count == 2 {
+      source = args[1]
+      
+      if source == "-h" {
+        print("Usage: ./\"PL meds DB\" [datasource] [-h]")
+        print("                       datasource - (changes latest [fast] full)")
+        return
+      }
+    }
+    let theUrl = URL(string: dataSource(source))
+    let xml = try! XMLDocument.init(contentsOf: theUrl!, options: 0)
+    list = MedsParser(xml).parse()
+    
+    print("Znaleziono: \(list.count) leków")
+  }
+  
+  func dataSource(_ source: String) -> String {
+    switch source {
+    case "changes":
+      return "http://pub.rejestrymedyczne.csioz.gov.pl/pobieranie_WS/Pobieranie.ashx?filetype=XMLUpdateFile&regtype=RPL_FILES_GROWTH"
+    case "latest":
+      return "http://pub.rejestrymedyczne.csioz.gov.pl/pobieranie_WS/Pobieranie.ashx?filetype=XMLFile&regtype=RPL_FILES_BASE"
+    case "fast", "default":
+      return "\(self.basePath)/samples/sample.xml"
+    case "slow", "full":
+      return "\(self.basePath)/samples/full-sample.xml"
+    default:
+      fputs("ERROR: unsupported datasource parameter", stderr)
+      exit(1)
+    }
+  }
+}
 
-//var fileUrl = "http://pub.rejestrymedyczne.csioz.gov.pl/pobieranie_WS/Pobieranie.ashx?filetype=XMLUpdateFile&regtype=RPL_FILES_GROWTH"
-var basePath = "file:///Users/ravbaker/Code/Home-First-Aid-Kit"
-var fileUrl = "\(basePath)/samples/sample.xml"
 
-let theUrl = URL(string: fileUrl)
-let xml = try XMLDocument.init(contentsOf: theUrl!, options: 0)
-let list = MedsParser(xml).parse()
-
-print("Znaleziono: \(list.count) leków")
-
+let app = ConsoleApp(CommandLine.arguments)
